b7dfc9b5d5bd6e4b3d84671a51c9284881551518
김민지 <happyhill99@gmail.com>
2023-07-10 20:46:04 +0900

    leetcode

---
+++ 21.Merge Two Sorted Lists.py
@@ -0,0 +1,13 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        
+
+s=Solution()
+
+c=ListNode(4,None)
+b=ListNode(2,c)
+list1=ListNode()
