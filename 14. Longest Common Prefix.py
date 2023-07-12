1cda408c9a15ce40f8eb76a92830d81084cebbcd
김민지 <happyhill99@gmail.com>
2023-07-12 17:19:29 +0900

    Update 14. Longest Common Prefix.py

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -5,4 +5,23 @@ Output: "fl"
 
 class Solution:
     def longestCommonPrefix(self, strs):
-        
+        min_length=0
+        min_len=min(len(i) for i in strs)
+        for w in strs:
+            w = w[:min_len]
+            
+        if len[i] < len[i+1] :
+            max_length = len[i+1]
+        else:
+            max_length = len[i]
+            
+        for j in range(max_length):
+            if i[j] == strs[j][j]:
+                continue
+        return ''
+
+
+s=Solution()
+
+print(s.longestCommonPrefix(["flower","flow","flight"]))
+      
